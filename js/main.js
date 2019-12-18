094ce6d65f447148fd3b07365da76f6173542756
draZer0 <ardkokkel@outlook.com>
2019-12-18 14:46:19 +0100

    BPI api from coindesk connect to get price

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 window.onload = () => {
+//inner
 
-  console.log("Hello World");
-}
+  const button = document.getElementById('js--button');
+  const span = document.getElementById('js--btc');
+
+
+  button.onclick = (event) => {
+    getBtc();
+  }
+
+  const getBtc = () => {
+    const BASE_URL = 'https://api.coindesk.com/v1/bpi/currentprice/EUR.json';
+
+    fetch(BASE_URL)
+    .then((data) => {
+      return data.json();
+    })
+    .then((response) => {
+      span.innerHTML = response.bpi.EUR.rate;
+    })
+  }
+
+//onload
+};
